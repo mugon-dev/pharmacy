786d0e3fef5894cd1bb5dd174f4828bdc4412796
conny <jmg41490400@gmail.com>
2023-01-15 16:39:49 +0900

    PRS-20 kakao 주소검색 api 테스트 케이스 작성

--- src/test/groovy/com/fastcampus/pharmacy/pharmacy/repository/PharmacyRepositoryTest.groovy
+++ src/test/groovy/com/fastcampus/pharmacy/pharmacy/repository/PharmacyRepositoryTest.groovy
@@ -8,6 +8,10 @@ class PharmacyRepositoryTest extends AbstractIntegrationContainerBaseTest {
     @Autowired
     private PharmacyRepository pharmacyRepository
 
+    def setup() {
+        pharmacyRepository.deleteAll()
+    }
+
     def "PharmacyRepository save"() {
         given:
         String address = "서울 특별시 성복구 중앙동"
@@ -29,5 +33,25 @@ class PharmacyRepositoryTest extends AbstractIntegrationContainerBaseTest {
         result.getPharmacyName() == name
         result.getLatitude() == latitude
         result.getLongitude() == longitude
+    }
+
+    def "PharmacyRepository saveAll"() {
+        given:
+        String address = "서울 특별시 성복구 중앙동"
+        String name = "은혜 약국"
+        double latitude = 36.11
+        double longitude = 128.11
+        def pharmacy = Pharmacy.builder()
+                .pharmacyAddress(address)
+                .pharmacyName(name)
+                .latitude(latitude)
+                .longitude(longitude)
+                .build()
+        when:
+        pharmacyRepository.saveAll(Arrays.asList(pharmacy))
+        def result = pharmacyRepository.findAll()
+
+        then:
+        result.size() == 1
     }
 }
