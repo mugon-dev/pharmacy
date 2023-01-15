195bf3a3c7e08797313ff0d126bd358dae855442
conny <jmg41490400@gmail.com>
2023-01-15 15:50:35 +0900

    PRS-18 Testcontainers 소개

---
+++ src/main/java/com/fastcampus/pharmacy/pharmacy/repository/PharmacyRepository.java
@@ -0,0 +1,8 @@
+package com.fastcampus.pharmacy.pharmacy.repository;
+
+import com.fastcampus.pharmacy.pharmacy.entity.Pharmacy;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PharmacyRepository extends JpaRepository<Pharmacy, Long> {
+
+}
