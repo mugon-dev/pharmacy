110bb2c5e2244a19cb9b6a3ec092e519849835ee
conny <jmg41490400@gmail.com>
2023-02-16 11:58:17 +0900

    handlebars 적용

---
+++ src/main/java/com/fastcampus/pharmacy/direction/controller/FormController.java
@@ -0,0 +1,15 @@
+package com.fastcampus.pharmacy.direction.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class FormController {
+
+  @GetMapping("/")
+  public String main() {
+    return "main";
+  }
+}
